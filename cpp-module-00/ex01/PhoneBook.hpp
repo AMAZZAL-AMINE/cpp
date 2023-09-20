e2229e3bb2c3aeacc9ba2bfd598ae5df6b99a285
Mohamed Amine Amazzal <mamazzal@c1r9p1.1337.ma>
2023-09-20 21:08:55 +0100

    init search query

--- cpp-module-00/ex01/PhoneBook.hpp
+++ cpp-module-00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 19:13:01 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/09/20 19:50:20 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/09/20 21:08:27 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,22 @@ class PhoneBook {
         std::cout << "_____________________________________________" << std::endl;
         count++;
       }
+    }
+    void SearchForContact(int searchQuery) {
+        int searchCount = 0;
+        if (searchQuery >= this->index || (is_full != 0 && searchQuery > 8)) {
+          std::cout << "Bro Range Not found" << std::endl;
+        }
+        while (searchCount < this->index || (is_full != 0 && searchCount < 8)) {
+          if (searchCount == searchQuery) {
+            std::cout << "_____________________________________________" << std::endl;
+            std::cout << "ID | NAME | PHONE " << std::endl;
+            std::cout << "_____________________________________________" << std::endl;
+            std::cout << " " << searchCount << " | " << this->contacts[searchCount].getName() << " | " << this->contacts[searchCount].getPhone() << std::endl;
+            break;
+          }
+          searchCount++;
+        }
     }
 };
 
--- cpp-module-00/ex01/main.cpp
+++ cpp-module-00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 18:29:43 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/09/20 20:03:18 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/09/20 21:03:23 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,43 @@
 std::string  ReadInputName(std::string name) {
   std::cout << "Enter Contact Name : ";
   std::getline(std::cin, name);
-  std::endl(std::cout);
   return name;
 }
 std::string  ReadInputPhone(std::string phone) {
   std::cout << "Enter Phome number : ";
   std::getline(std::cin, phone);
-  std::endl(std::cout);
   return phone;
+}
+
+int FieldIsEmpty(std::string field) {
+  if (field.empty()) {
+    std::cout << "Field is empty, try again" << std::endl;
+    return 1;
+  }
+  return 0;
 }
 
 int main() {
   PhoneBook phoneBook;
-  std::string name, phone, command;
+  std::string name, phone, command, ContactId;
+
   while (command != "EXIT") {
     while (command == "ADD") {
       name  = ReadInputName(name);
+      if (FieldIsEmpty(name))
+        break;
       phone = ReadInputPhone(phone);
+      if (FieldIsEmpty(phone))
+        break;
       phoneBook.AddContact(Contact(name, phone)); 
       command = "";
     }
     if (command == "SHOW") {
       phoneBook.DisplayContacts();
+    }else if (command == "SEARCH") {
+      std::cout << "Enter Contact ID : ";
+      std::getline(std::cin, ContactId);
+      phoneBook.SearchForContact(std::stoi(ContactId));
     }
     std::cout << "Enter you command : " ;
     std::getline(std::cin, command);
