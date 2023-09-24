b26101c124c5bde6adad2572f540a6c328cfaf68
Mohamed Amine Amazzal <mamazzal@c1r2p2.1337.ma>
2023-09-24 19:58:40 +0100

    fix some issue in ex05

--- cpp-module-01/ex05/main.cpp
+++ cpp-module-01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 17:22:39 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/09/24 19:49:30 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/09/24 19:56:56 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,13 @@
 int main() {
   Harl *harlObj = new Harl;
   std::string input;
-  
-  while (true) {
-    std::cout << "MOOD : ";
-    std::getline(std::cin, input);
-    if (input.empty()) {
-      std::cout << "Input Cant be empty" << std::endl;
-      break;
-    }
-    else {
-      harlObj->complain(input);
-    }
+
+  std::cout << "PUT THE HARL COMMANDE : ";
+  std::getline(std::cin, input);
+  if (input.empty()) {
+    std::cout << "Input Cant be empty" << std::endl;
+    return (0);
   }
+  harlObj->complain(input);
   return 0;
 }
