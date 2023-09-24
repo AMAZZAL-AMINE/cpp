0ed1417d05548bb26295dd4dd51b60a18b60f7f0
Mohamed Amine Amazzal <mamazzal@c1r2p2.1337.ma>
2023-09-24 20:00:29 +0100

    init ex06

---
+++ cpp-module-01/ex06/Harl.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/24 17:34:33 by mamazzal          #+#    #+#             */
+/*   Updated: 2023/09/24 18:12:04 by mamazzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include "main.h"
+
+class Harl {
+  private :
+    void debug(void);
+    void info(void);
+    void warning(void);
+    void error(void);
+  public :
+    Harl();
+    void complain(std::string level);
+    ~Harl();
+};
+
+#endif
