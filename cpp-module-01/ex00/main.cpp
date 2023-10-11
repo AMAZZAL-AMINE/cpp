ee845ab0752b98382e03ab49e8512a796b447187
Mohamed Amine Amazzal <mamazzal@c1r9p1.1337.ma>
2023-10-11 13:35:31 +0100

    fix ex00

--- cpp-module-01/ex00/main.cpp
+++ cpp-module-01/ex00/main.cpp
@@ -6,13 +6,18 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 16:46:55 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/09/21 20:14:27 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/10/11 13:32:37 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
 int main() {
-  randomChump("Zombie");
+  std::string name = "FOO";
+  std::string name2 = "BEE";
+  Zombie *zombie = newZombie(name);
+  zombie->announce();
+  randomChump(name2);
+  delete zombie;
   return 0;
 }
--- cpp-module-01/ex00/newZombie.cpp
+++ cpp-module-01/ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 17:13:50 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/09/21 19:45:03 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/10/11 13:31:00 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ Zombie* newZombie( std::string name)  {
 }
 
 void randomChump( std::string name ) {
-  Zombie *zombie = newZombie(name);
-  zombie->announce();
-  delete zombie;
+  Zombie zombie(name);
+  zombie.announce();
 }
