7f4d73be0dc3ec78f4e707e94789c28610777cd2
Mohamed Amine Amazzal <mamazzal@c1r2p7.1337.ma>
2023-10-26 14:23:42 +0100

    gg

--- cpp-module-04/ex02/Dog.cpp
+++ cpp-module-04/ex02/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 15:32:31 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/10/24 19:37:36 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/10/26 14:21:54 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,31 +19,31 @@ Dog::Dog()
 	this->brain = new Brain();
 }
 
-Dog::Dog( const Dog & src )
+Dog::Dog( const Dog & src)
 {
 	std::cout << "Dog copy constructor called" << std::endl;
-	*this = src;
+	this->brain = new Brain(*src.brain);
+	this->type = src.type;
 }
-
 
 Dog::~Dog()
 {
-	std::cout << "Dog destructor called" << std::endl;
 	delete this->brain;
+	std::cout << "Dog destructor called" << std::endl;
 }
 
-
-Dog &				Dog::operator=( Dog const & rhs )
+Dog &		Dog::operator=( Dog const &  rhs )
 {
-	std::cout << "Dog asignment operator called" << std::endl;
-	if ( this != &rhs )
-	{
+	std::cout << "Dog assignment operator called" << std::endl;
+	if (this != &rhs) {
+		delete this->brain;
 		this->type = rhs.type;
+		this->brain = new Brain(*rhs.brain);
 	}
-	return *this;
+  return *this;
 }
 
-void				Dog::makeSound() const
+void		Dog::makeSound() const
 {
-	std::cout << this->type << " maked a soung" << std::endl;
+	std::cout << this->type << " maked a sound" << std::endl;
 }
